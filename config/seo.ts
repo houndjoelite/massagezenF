be6072fd5a38ee32f6025db4eead6757bf0c7a33
houndjoelite <houndjojeanjacques82@gmail.com>
2026-03-15 16:08:56 +0100

    Add SEO configuration for MassageZen

---
+++ config/seo.ts
@@ -0,0 +1,9 @@
+export const seoConfig = {
+  siteName: "MassageZen",
+  siteUrl: "https://monappareildemassage.com",
+  defaultTitle: "MassageZen - Appareils de Massage Premium | Guides & Comparatifs",
+  defaultDescription: "Découvrez les meilleurs appareils de massage 2026 : pistolets, fauteuils, coussinets chauffants. Guides d'experts et comparatifs détaillés.",
+  defaultImage: "/og-image-massagezen.png",
+  twitterHandle: "@massagezen",
+  locale: "fr_FR",
+}
